43b81f8b9b38187d4e17f3534e2aab7d374eff53
bremerm <bremer.marten@web.de>
2020-06-10 18:04:47 +0200

    sds

---
+++ martentestet.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GWB - Wir bauen!</title>
+    	<link rel="stylesheet" type="text/css" href="martentestet.css">
+    </head>
+    <body>
+   	 	<form method="post">
+			<nav>
+			<ul id="header">
+				<li><a class="active" href="martentestet.php">Startseite</a></li>
+				<li><a href="Dienstleistungen.php">Dienstleistungen</a></li>
+				<li><a href="Projekte.php">Projekte</a></li>
+				<li><a href="Karriere.php">Karriere</a></li>
+				<li><a href="Ueber uns.php">Über uns</a></li>
+				<li style="float: right"><a href="Kontakt.php">Kontakt</a></li>
+			</ul>
+			</nav>
+			<div>
+				<h1>Test Überschrift</h1>
+			</div>
+			<div>
+				<p>Hallo Welt</p>
+				<div id="trenner"></div>
+				Hallo Fabian
+			</div>
+			<p>
+			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+			</p>
+    	</form>
+    </body>
+</html>
